88ec573ad4e860e34c3e78a3b4bd86757f82ced2
Kevin <kevinzhang@live.nl>
2019-10-31 12:42:50 +0100

    Add project files.

---
+++ GovernancePortal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GovernancePortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
